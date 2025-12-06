b514555fe41b5b3262dabdbd0b2d70fdb69b4106
Xitee <59659167+Xitee1@users.noreply.github.com>
2025-12-06 21:41:01 +0100

    Do not log to file

--- src/my_qbit_manager/main.py
+++ src/my_qbit_manager/main.py
@@ -15,8 +15,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler('logs/qbit_manager.log')
+        logging.StreamHandler(sys.stdout)
     ]
 )
 
